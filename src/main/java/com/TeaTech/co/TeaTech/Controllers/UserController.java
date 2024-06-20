136102e19a08eb9adfcffb39db8e491115a32c7e
Trixieee20 <trixieemacalalad@gmail.com>
2024-06-20 15:37:54 +0800

    configure to database

--- src/main/java/com/TeaTech/co/TeaTech/Controllers/UserController.java
+++ src/main/java/com/TeaTech/co/TeaTech/Controllers/UserController.java
@@ -1,6 +1,10 @@
 package com.TeaTech.co.TeaTech.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.TeaTech.co.TeaTech.Model.UserModel;
@@ -13,5 +17,17 @@ public class UserController {
         return new UserModel(1, "Trixie", "Trixiem@email.com", "Trixie20");
 
     }
-   
+    @GetMapping("/users")
+    public List<UserModel> getUsers(){
+        List<UserModel> users = new ArrayList<>();
+        users.add(new UserModel(1, "Trixie", "trixieemacalalad@gmail.com", "112002tam"));
+        users.add(new UserModel(1, "JohnRey", "johnreycatibog@gmail.com", "111402jmc"));
+        users.add(new UserModel(1, "MarkDaniel", "makealipustain@gmail.com", "011203mda"));
+        return users;
+
+    }
+    @GetMapping("/user/{name}")
+    public UserModel getUserFromName(@PathVariable("name")String name){
+        return new UserModel(1, "Trixie", "trixieemacalalad@gmail.com", "112002tam");
+    }
 }
